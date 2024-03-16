d1680ab692edda4ed2d665ae5196e031ad0c9ccc
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-03-16 20:21:46 +0300

    done with 5 passing 2 failing

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+class TestBook:
+    def title_and_page_count(self):
+        book = Book("And Then There Were None", 272)
+        assert book.page_count == 272
+        assert book.title == "And Then There Were None"
+
+    def int_page_count(self):
+        book = Book("And Then There Were None", 272)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        book.page_count = "not an integer"
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue().strip() == "page_count must be an integer\n"
+
+    def turn_page_output(self):
+        book = Book("The World According to Garp", 69)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        book.turn_page()
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue().strip() == "Flipping the page...wow, you read fast!"
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = 'Used'
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'
+
+class TestShoe:
+    def has_brand_and_size(self):
+        stan_smith = Shoe("Adidas", 9)
+        assert stan_smith.brand == "Adidas"
+        assert stan_smith.size == 9
+
+    def requires_int_size(self):
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.size = "not an integer"
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue().strip() == "size must be an integer"
+
+    def can_cobble(self):
+        stan_smith = Shoe("Adidas", 9)
+        captured_out = io.StringIO()
+        sys.stdout = captured_out
+        stan_smith.cobble()
+        sys.stdout = sys.__stdout__
+        assert captured_out.getvalue().strip() == "Your shoe is as good as new!"
+
+    def cobble_makes_new(self):
+        stan_smith = Shoe("Adidas", 9)
+        stan_smith.cobble()
+        assert stan_smith.condition == "New"
